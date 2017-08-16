e439069c822b306e55149b1ee77565a67084dad8
veronicasyseo <veronica.seo@gmail.com>
2017-08-16 16:34:38 +0800

    first commit

---
+++ contracts/Lunch.sol
@@ -0,0 +1,65 @@
+contract Lunch {
+	
+	address public organizer;
+	mapping (address => bool) public
+	attendeeJoined;
+	uint public numOfAttendee;
+	uint public limit;
+	uint public totalCost;
+
+function Lunch() {
+	
+	organizer = msg.sender;
+	limit = 10;
+	numOfAttendee = 0;
+	totalCost = 100;
+
+}
+
+function changeTotalCost(uint
+newTotalCost) public {
+	
+	if (msg.sender != organizer) {
+	return; }
+	totalCost = newTotalCost;
+
+}
+
+function attend() public payable
+returns(bool) {
+
+	if (numOfAttendee >= limit) {
+	return false; }
+	attendeeJoined[msg.sender] = true;
+	numOfAttendee++;
+	return True;
+
+}
+
+funtion getAvailable() public returns (uint) {
+	
+	return limit - numbOfAttendee;
+
+}
+
+function checkAttendance(address
+people) public returns (bool) {
+	
+	return attendeeJoined[people];
+}
+
+function splitCost() public returns (uint) {
+	
+	return totalCost / numOfAttendee;
+
+}
+
+function kill() {
+	
+	if (msg.sender == organizer) {
+
+		suicide(organizer);
+		
+	}
+
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,17 @@
+//var ConvertLib = artifacts.require("./ConvertLib.sol");
+//var MetaCoin = artifacts.require("./MetaCoin.sol");
+
+//module.exports = function(deployer) {
+  //deployer.deploy(ConvertLib);
+  //deployer.link(ConvertLib, MetaCoin);
+  //deployer.deploy(MetaCoin);
+//};
+
+var Lunch =
+artifacts.require("./Lunch.sol");
+
+module.exports = function(deployer) {
+
+	deployer.deploy(Lunch);
+
+};
